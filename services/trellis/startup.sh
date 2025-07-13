c3c4d5dc585580b83f790cb0bcabf13f257766df
Brian Gebel <Brian Gebel>
2025-07-12 19:45:10 -0700

    fix build

--- services/trellis/startup.sh
+++ services/trellis/startup.sh
@@ -2,15 +2,12 @@
 
 source ~/.venv/bin/activate
 
-# Run post install steps if they haven't been run yet
-./post_install.sh
-
-# export CXX=/usr/local/bin/gxx-wrapper
-
 echo "Starting TRELLIS on $GRADIO_SERVER_NAME:$GRADIO_SERVER_PORT"
 # Set Gradio environment variables to ensure proper binding
 export GRADIO_SERVER_NAME=$GRADIO_SERVER_NAME
 export GRADIO_SERVER_PORT=$GRADIO_SERVER_PORT
-python3 launch_app.py
+
+# Launch TRELLIS application
+python3 app.py
 
 echo "Application exited unexpectedly."
